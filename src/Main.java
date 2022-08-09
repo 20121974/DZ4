4a4141a28a696f5f9b72db47332dec948afb54bb
1 <nfi46102@mail.ru>
2022-08-09 16:37:59 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,29 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        //DZ 4.2
+
+        //DZ 4.1
        int number = 1;
        while (number <= 10) {
            System.out.print(" " + number);
            number ++;}
        System.out.println("");
-
        for (int numberNew = number - 1; numberNew >=1; numberNew --){
-           System.out.print(" " + numberNew);
-       }
+           System.out.print(" " + numberNew);}
+
        //DZ 4.2
+        System.out.println("");
+        for (int friday = 5; friday <= 31;  friday ++) {
+            friday = friday + 7;
+            System.out.println("Сегодня пятница " + friday + "-е число. Необходимо подготовить отчет.");}
+
+        //DZ 4.3
+        int years = 2017;
+        int yearsStart = years - 200;
+        int yearsFinish = years + 100;
+        while (yearsStart < yearsFinish) {
+            System.out.println(yearsStart);
+            yearsStart = yearsStart + 79;}
 
     }
 }
